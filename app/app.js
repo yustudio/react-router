371dd9c9a6793d939be602b8b792b84c40fe7638
desongyu <desong.e.yu@gmail.com>
2016-12-27 14:54:54 -0500

    test

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ class App extends Component {
 }
 
 const NotFound = () => ( <h1> 404... page not found! </h1>)
-const Home = () => <h1> Hello from Home! </h1>
+const Home = () => <h1> Hello from Home!! </h1>
 //const Address = () => <h1> We are located at 555 Jackson St </h1>
 const Address = (props) => ( 
 	<div>   
